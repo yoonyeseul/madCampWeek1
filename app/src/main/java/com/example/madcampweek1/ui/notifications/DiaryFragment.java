7c0a6ce63dfca602a25e7147386678676488091b
yoonyeseul <yoonyesle@naver.com>
2022-01-04 17:35:05 +0900

    fragment가 create 되었을 때 작성된 일기가 없는 경우에 해당하는 TextView가 뜨지 않는 문제 해결. text == "" 대신 text.length() == 0 로 바꿈.

--- app/src/main/java/com/example/madcampweek1/ui/notifications/DiaryFragment.java
+++ app/src/main/java/com/example/madcampweek1/ui/notifications/DiaryFragment.java
@@ -20,40 +20,12 @@ import java.io.File;
 import java.io.FileReader;
 
 public class DiaryFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
     public String text;
-
     public View root;
     private TextView diaryTextView;
 
     public DiaryFragment() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment DiaryFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static DiaryFragment newInstance(String param1, String param2) {
-        DiaryFragment fragment = new DiaryFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
     }
 
     @Override
@@ -64,11 +36,10 @@ public class DiaryFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         root = inflater.inflate(R.layout.fragment_diary, container, false);
         diaryTextView = root.findViewById(R.id.diaryTextViewInFragment);
         textViewWhenNoDiary = root.findViewById(R.id.textViewWhenNoDiary);
-        if (text == "") {
+        if (text.length() == 0) {
             textViewWhenNoDiary.setVisibility(View.VISIBLE);
             diaryTextView.setVisibility(View.INVISIBLE);
         }
@@ -76,8 +47,6 @@ public class DiaryFragment extends Fragment {
             textViewWhenNoDiary.setVisibility(View.INVISIBLE);
             diaryTextView.setText(text);
         }
-
-        System.out.println("Diary Fragment : onCreateView() 호출됨");
         return root;
     }
     private TextView textViewWhenNoDiary;
